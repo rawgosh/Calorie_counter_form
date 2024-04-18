6ed24358361a96abf440b3c5fdcad532dac3d123
rawgosh <rawgoshshrestha129@gmail.com>
2024-04-18 23:27:16 +0545

    some functions

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const calorieCounter = document.getElementById("calorie-counter");
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+let isError = false
+
+function cleanInputString(str) {
+    //console.log("original string: " , str);
+    const regex = /[+-\s]/g; // g = global, s = whitespace
+    return str.replace(regex, '');
+  }
+//console.log(cleanInputString("+-  99"));
+function isInvalidInput(str) {
+    const regex = /[0-9]+e\d+/i; //i = insensetive (uppercase or lowercase) d = digit [0-9]
+    return str.match(regex);
+}
+//console.log(isInvalidInput("1e3"));
+function addEntry() {
+    const targetId = "#" +entryDropdown.value;
+    const targetInputContainer = document.querySelector(`${targetId}+' .input-container'`);
+}
